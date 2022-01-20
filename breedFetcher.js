a23b4af5f780d750b0d10fd6a6f9e8cd862188ba
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-01-20 16:03:02 -0500

    took care of edge cases of errors and breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,8 +2,19 @@ const request = require('request');
 const args = process.argv[2];
 
 request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
-  const data = JSON.parse(body)
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  console.log(data["0"].description);
+  const data = JSON.parse(body);
+  if (error === undefined) {
+    console.error('Error', error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error('Error');
+    return;
+  }
+  if (data.length === 0) {
+    console.log(`Breed not found when searching for "${args}".`);
+    return;
+  } else {
+    console.log(data["0"].description);
+  }
 });
