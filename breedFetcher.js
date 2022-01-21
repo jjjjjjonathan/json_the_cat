79f3df20995fad2442fffa2241a565e206b327f1
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2022-01-20 20:38:18 -0500

    implemented mocha tests to confirm everything works

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,30 +1,10 @@
 const request = require('request');
 
-// const args = process.argv[2];
-
-// request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (_error, response, body) => {
-//   const data = JSON.parse(body);
-//   if (error === undefined) {
-//     console.error('Error', error);
-//     return;
-//   }
-//   if (response.statusCode !== 200) {
-//     console.error('Error');
-//     return;
-//   }
-//   if (data.length === 0) {
-//     console.log(`Breed not found when searching for "${args}".`);
-//     return;
-//   } else {
-//     console.log(data["0"].description);
-//   }
-// });
-
 const fetchBreedDescription = (breedName, callback) => {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (_error, _response, body) => {
     const data = JSON.parse(body);
     if (data.length === 0) {
-      return `Breed not found when searching for "${breedName}"`;
+      return callback(`Breed not found when searching for "${breedName}"`, null);
     } else {
       return callback(null, data["0"].description);
     }
